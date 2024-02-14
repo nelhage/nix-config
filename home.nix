9e24c4bc3caa471af42ec81f96b7f8d6cf882b1f
Nelson Elhage <nelhage@nelhage.com>
2024-02-14 09:24:43 -0600

    import home-manager config

---
+++ home.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  home = {
+    username = "nelhage";
+    homeDirectory = if pkgs.stdenv.isLinux then "/home/nelhage" else  "/Users/nelhage";
+
+    stateVersion = "23.11";
+    packages = with pkgs; [
+      pv
+
+      # N.B. see https://nixos.wiki/wiki/Google_Cloud_SDK
+      google-cloud-sdk
+
+      hugo
+      gh
+    ];
+  };
+
+  programs.home-manager.enable = true;
+}
