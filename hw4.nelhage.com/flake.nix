cb2ffd0a9fa47c4b7b8e6fb8a8f906e99ac27ea0
Nelson Elhage <nelhage@nelhage.com>
2024-02-20 08:58:21 -0800

    oops, hardware config

--- hw4.nelhage.com/flake.nix
+++ hw4.nelhage.com/flake.nix
@@ -3,14 +3,16 @@
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { nixpkgs, disko, ... }:
-    {
-      nixosConfigurations.hw4 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-        ];
-      };
+  outputs = { nixpkgs, disko, ... }: {
+    # TODO: https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-fmt
+
+    nixosConfigurations.hw4 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        disko.nixosModules.disko
+        ./configuration.nix
+        ./hardware-configuration.nix
+      ];
     };
+  };
 }
