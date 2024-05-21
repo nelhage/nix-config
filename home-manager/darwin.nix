6c775033570265d8926747a0e6725edb10e761c5
Nelson Elhage <nelhage@nelhage.com>
2024-05-21 10:44:27 -0700

    Add hugo services

--- home-manager/darwin.nix
+++ home-manager/darwin.nix
@@ -1,4 +1,9 @@
-{ pkgs, nixpkgs, ... }:
+{
+  pkgs,
+  nixpkgs,
+  config,
+  ...
+}:
 {
   home.homeDirectory = "/Users/nelhage";
 
@@ -27,4 +32,56 @@
       executable = true;
     };
   };
+
+  launchd.agents =
+    let
+      logdir = "${config.home.homeDirectory}/Library/Logs/";
+    in
+    {
+      "hugo-notes" = {
+        enable = true;
+        config = {
+          Label = "com.nelhage.hugo-notes";
+
+          ProgramArguments = [
+            "${pkgs.hugo}/bin/hugo"
+            "serve"
+            "-D"
+            "--port"
+            "1987"
+            "--disableFastRender"
+            "--destination"
+            "_preview"
+          ];
+
+          WorkingDirectory = "${config.home.homeDirectory}/code/writing";
+          KeepAlive = true;
+          RunAtLoad = true;
+
+          StandardErrorPath = "${logdir}/hugo/notes-stdout.log";
+          StandardOutPath = "${logdir}/hugo/notes-stderr.log";
+        };
+      };
+
+      "hugo-blog" = {
+        enable = true;
+        config = {
+          Label = "com.nelhage.hugo-blog";
+
+          ProgramArguments = [
+            "${pkgs.hugo}/bin/hugo"
+            "serve"
+            "-D"
+            "--destination"
+            "_preview"
+          ];
+
+          WorkingDirectory = "${config.home.homeDirectory}/code/blog.nelhage.com";
+          KeepAlive = true;
+          RunAtLoad = true;
+          StandardErrorPath = "${logdir}/hugo/stdout.log";
+          StandardOutPath = "${logdir}/hugo/stderr.log";
+        };
+      };
+    };
 }
