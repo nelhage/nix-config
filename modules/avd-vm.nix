7241dc6f7071e5b08f33be7ea70403c79f71d4dc
Nelson Elhage <nelhage@nelhage.com>
2024-09-09 07:29:26 -0700

    avd-vm: ssh

--- modules/avd-vm.nix
+++ modules/avd-vm.nix
@@ -6,10 +6,28 @@
         target = "/mnt/avd";
       };
     };
+
+    forwardPorts = [
+      {
+        from = "host";
+        guest.port = 22;
+        host.port = 33322;
+      }
+    ];
   };
 
   environment.defaultPackages = with pkgs; [
     file
     qemu
+    unixtools.xxd
   ];
+
+  users.users.nelhage = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOj/9YTjI5Pr3TrzFMr9ADLTw7yeJZ6jCejXRL9N0rku nelhage@mythique"
+    ];
+  };
+
+  services.openssh.enable = true;
 }
