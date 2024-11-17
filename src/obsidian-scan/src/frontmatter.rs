17bc926a4a02a85a5d95c03ab7b781bbfa7dd463
Nelson Elhage <nelhage@nelhage.com>
2024-11-17 14:05:02 -0800

    parsing frontmatter

--- src/obsidian-scan/src/frontmatter.rs
+++ src/obsidian-scan/src/frontmatter.rs
@@ -1,9 +1,15 @@
-use anyhow::{anyhow, bail, Result};
+use anyhow::{anyhow, Result};
 use lazy_static::lazy_static;
 use regex::Regex;
+use saphyr::Yaml;
 use std::option::Option;
+use std::vec::Vec;
 
-pub struct FrontMatter {}
+#[derive(Debug, Default)]
+pub struct FrontMatter {
+    pub tags: Vec<String>,
+    pub aliases: Vec<String>,
+}
 
 lazy_static! {
     static ref YAML_DELIM: Regex = Regex::new(r"(?m)^---\r?\n").unwrap();
@@ -21,9 +27,37 @@ fn find_frontmatter(note: &str) -> Option<&str> {
     return frontmatter;
 }
 
+pub fn yaml_to_strvec(yaml: &Yaml) -> Option<Vec<String>> {
+    let vec = yaml.as_vec()?;
+    Some(
+        vec.into_iter()
+            .filter_map(|e| e.as_str())
+            .map(|e| e.to_owned())
+            .collect(),
+    )
+}
+
 pub fn extract_frontmatter(text: &str) -> Result<FrontMatter> {
     let yaml_text = find_frontmatter(text).ok_or(anyhow!("No front matter found"))?;
-    bail!("unimplemented")
+    let yaml = Yaml::load_from_str(yaml_text)?;
+    let doc = yaml[0]
+        .as_hash()
+        .ok_or(anyhow!("frontmatter document is not a object"))?;
+    let tags = doc
+        .get(&Yaml::String("tags".to_owned()))
+        .and_then(|doc| yaml_to_strvec(doc))
+        .unwrap_or(Vec::new());
+    let mut aliases = doc
+        .get(&Yaml::String("aliases".to_owned()))
+        .and_then(|doc| yaml_to_strvec(doc))
+        .unwrap_or(Vec::new());
+    if let Some(alias) = doc
+        .get(&Yaml::String("alias".to_owned()))
+        .and_then(|e| e.as_str())
+    {
+        aliases.push(alias.to_owned());
+    }
+    Ok(FrontMatter { tags, aliases })
 }
 
 #[cfg(test)]
@@ -31,7 +65,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_findfrontmatter() {
+    fn test_find_frontmatter() {
         assert_eq!(
             find_frontmatter(
                 r#"---
@@ -84,5 +118,31 @@ key: value
             ),
             None
         );
+    }
+
+    #[test]
+    fn test_extract_frontmatter() {
+        let front = extract_frontmatter(
+            r#"---
+tags: [lemons, {}, cars]
+---
+some nonsense
+"#,
+        )
+        .unwrap();
+        assert_eq!(front.tags, vec!("lemons".to_owned(), "cars".to_owned()));
+
+        let front = extract_frontmatter(
+            r#"---
+tags: [lemons, {}, cars]
+aliases: [otherdoc, newdoc]
+alias: Maria
+---
+some nonsense
+"#,
+        )
+        .unwrap();
+        assert_eq!(front.tags, vec!("lemons", "cars"));
+        assert_eq!(front.aliases, vec!("otherdoc", "newdoc", "Maria"));
     }
 }
