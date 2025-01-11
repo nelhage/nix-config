7139e45628500a888fa35c97adfdf8c94a461e2f
Nelson Elhage <nelhage@nelhage.com>
2025-01-11 00:48:42 +0000

    Add sha for x86_64-linux

--- pkgs/garmindb/default.nix
+++ pkgs/garmindb/default.nix
@@ -5,6 +5,7 @@ callPackage ../../lib/build-uv-package.nix {
   src = ./.;
 
   packagesHash = {
+    x86_64-linux = "sha256-w1/4UmHic3sVlcX/WPwWcPVuP6b6cfDGXpBEd5okYTo=";
     aarch64-darwin = "sha256-Lrz5/5ZAhUjsdXLSJBNz12MRUKIOt90EaPWuXNnHw5Y=";
   };
 
