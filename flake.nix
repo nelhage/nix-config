f2b3470d91a1b5b8591e67d9df1e5991452f1d66
Nelson Elhage <nelhage@nelhage.com>
2025-01-15 12:43:45 -0800

    refactor top-level flake
    Use `self` to simplify a bit and be more consistent

--- flake.nix
+++ flake.nix
@@ -34,6 +34,9 @@
         "x86_64-linux"
         "aarch64-darwin"
       ];
+      subdirs = [
+        ./src/obsidian-scan
+      ];
       darwinRevisionConfig = {
         system.configurationRevision = self.rev or self.dirtyRev or null;
       };
@@ -43,85 +46,87 @@
           host.pkgs = nixpkgs.legacyPackages.aarch64-darwin;
         };
       };
-      obsidian-scan = import ./src/obsidian-scan inputs;
       specialArgs = {
         inherit inputs;
       };
       lib = nixpkgs.lib;
+      outputs = {
+        darwinConfigurations."mythique" = nix-darwin.lib.darwinSystem {
+          inherit specialArgs;
+          modules = [
+            self.nixosModules.overlays
+            home-manager.darwinModules.default
+            darwinRevisionConfig
+            ./darwin/mythique.nix
+          ];
+        };
+
+        nixosConfigurations.hw4 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          inherit specialArgs;
+          modules = [
+            self.nixosModules.overlays
+            disko.nixosModules.disko
+            home-manager.nixosModules.default
+
+            ./modules/nelhage.com.nix
+            ./modules/common.nix
+            ./hw4.nelhage.com/configuration.nix
+            ./hw4.nelhage.com/hardware-configuration.nix
+          ];
+        };
+
+        nixosConfigurations.avdVM = nixpkgs.lib.nixosSystem {
+          system = "aarch64-linux";
+          inherit specialArgs;
+          modules = [
+            darwinVMHost
+            ./modules/vm-base.nix
+            ./modules/avd-vm.nix
+          ];
+        };
+
+        nixosModules.overlays = {
+          nixpkgs.overlays = [
+            agenix.overlays.default
+            self.overlays.default
+          ];
+        };
+
+        overlays.default = final: prev: {
+          base16-shell = self.packages.${prev.system}.base16-shell;
+          obsidian-scan = self.packages.${prev.system}.obsidian-scan;
+        };
+
+        packages = forAllSystems (
+          system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            base16-shell = pkgs.callPackage ./pkgs/base16-shell.nix { };
+            garmindb = pkgs.callPackage ./pkgs/garmindb { python = pkgs.python312; };
+          }
+        );
+
+        devShells = forAllSystems (
+          system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            rules_boost = pkgs.callPackage ./shells/rules_boost.nix { };
+          }
+        );
+
+        templates.default = {
+          path = ./template;
+          description = "Development template";
+          welcomeText = "Add your packages to flake.nix";
+        };
+      };
     in
-    lib.attrsets.recursiveUpdate rec {
-      darwinConfigurations."mythique" = nix-darwin.lib.darwinSystem {
-        inherit specialArgs;
-        modules = [
-          nixosModules.overlays
-          home-manager.darwinModules.default
-          darwinRevisionConfig
-          ./darwin/mythique.nix
-        ];
-      };
-
-      nixosConfigurations.hw4 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        inherit specialArgs;
-        modules = [
-          nixosModules.overlays
-          disko.nixosModules.disko
-          home-manager.nixosModules.default
-
-          ./modules/nelhage.com.nix
-          ./modules/common.nix
-          ./hw4.nelhage.com/configuration.nix
-          ./hw4.nelhage.com/hardware-configuration.nix
-        ];
-      };
-
-      nixosConfigurations.avdVM = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        inherit specialArgs;
-        modules = [
-          darwinVMHost
-          ./modules/vm-base.nix
-          ./modules/avd-vm.nix
-        ];
-      };
-
-      nixosModules.overlays = {
-        nixpkgs.overlays = [
-          agenix.overlays.default
-          overlays.default
-          obsidian-scan.overlays.obsidian-scan
-        ];
-      };
-
-      overlays.default = final: prev: {
-        base16-shell = packages.${prev.system}.base16-shell;
-      };
-
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          base16-shell = pkgs.callPackage ./pkgs/base16-shell.nix { };
-          garmindb = pkgs.callPackage ./pkgs/garmindb { python = pkgs.python312; };
-        }
-      );
-
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          rules_boost = pkgs.callPackage ./shells/rules_boost.nix { };
-        }
-      );
-
-      templates.default = {
-        path = ./template;
-        description = "Development template";
-        welcomeText = "Add your packages to flake.nix";
-      };
-    } obsidian-scan;
+    builtins.foldl' lib.attrsets.recursiveUpdate outputs (
+      builtins.map (dir: import dir inputs) subdirs
+    );
 }
