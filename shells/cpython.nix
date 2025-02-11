d156e877cc5923bd7b3a80d8f59e089f00376903
Nelson Elhage <nelhage@nelhage.com>
2025-02-11 20:03:46 +0000

    Add LLVM binutils for cpython

--- shells/cpython.nix
+++ shells/cpython.nix
@@ -4,5 +4,6 @@
   llvmPackages_19,
 }:
 mkShell.override ({ stdenv = llvmPackages_19.stdenv; }) {
-  packages = [ ] ++ python3.buildInputs ++ python3.nativeBuildInputs;
+  packages =
+    [ llvmPackages_19.bintools-unwrapped ] ++ python3.buildInputs ++ python3.nativeBuildInputs;
 }
