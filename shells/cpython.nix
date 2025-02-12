9e8bf9931db1cced9b2536c803bd62af3d9b2f0b
Nelson Elhage <nelhage@nelhage.com>
2025-02-12 23:21:53 +0000

    cpython: Both GCC and clang shells

--- shells/cpython.nix
+++ shells/cpython.nix
@@ -3,7 +3,14 @@
   python3,
   llvmPackages_19,
 }:
-mkShell.override ({ stdenv = llvmPackages_19.stdenv; }) {
-  packages =
-    [ llvmPackages_19.bintools-unwrapped ] ++ python3.buildInputs ++ python3.nativeBuildInputs;
+let
+  pkgs = python3.buildInputs ++ python3.nativeBuildInputs;
+in
+mkShell {
+  packages = pkgs;
+  passthru = {
+    clang = mkShell.override ({ stdenv = llvmPackages_19.stdenv; }) {
+      packages = pkgs ++ [ llvmPackages_19.bintools-unwrapped ];
+    };
+  };
 }
