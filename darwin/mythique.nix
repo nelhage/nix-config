533b05e9cf1ec9d15b43d5a79253908708ec6ac2
Nelson Elhage <nelhage@nelhage.com>
2025-02-19 15:08:19 -0800

    litestream config file on mythique

--- darwin/mythique.nix
+++ darwin/mythique.nix
@@ -11,6 +11,12 @@
   home-manager.users.nelhage =
     { ... }:
     {
+      imports = [
+        ../home-manager/litestream.nix
+      ];
+
+      xdg.configFile."litestream-garmin.yml".source = ./litestream-garmin.yml;
+
       sync.autocommit.enable = true;
     };
 }
