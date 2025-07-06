bb6b0a83267dd6c9f13977a508786ec483e17677
Nelson Elhage <nelhage@nelhage.com>
2025-07-06 11:19:53 -0700

    We no longer need this
    I fixed $PATH so that the nix git comes first.

--- template/flake.nix
+++ template/flake.nix
@@ -21,10 +21,6 @@
           default = pkgs.mkShell {
             packages = [
             ];
-            shellHook = ''
-                # Git on macos misbehaves when this is set.
-                unset DEVELOPER_DIR
-            '';
           };
         }
       );
