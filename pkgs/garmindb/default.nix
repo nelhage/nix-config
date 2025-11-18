0932c93d93a772fcc9a5558f24b0301819346f27
Nelson Elhage <nelhage@nelhage.com>
2025-11-17 20:21:21 -0800

    Add pandas&PyArrow to garmindb environment

--- pkgs/garmindb/default.nix
+++ pkgs/garmindb/default.nix
@@ -1,14 +1,21 @@
-{ callPackage, python3, ... }:
+{
+  callPackage,
+  python3,
+  lib,
+  libz,
+  ...
+}:
 callPackage ../../lib/build-uv-package.nix {
   name = "garmin";
   version = "0.1.0";
   src = ./.;
 
-  packagesHash = {
-    x86_64-linux = "sha256-3OYbyp9n2NJJn5eUsBZV+RA0cwbx1nGyIdU5/j8+n/w=";
-    aarch64-darwin = "sha256-/dIN7q4JfnBU/cr1iAPUyfm0Pn6b2eJCA/1m4QQMLvY=";
-  };
+  packagesHash = lib.trivial.importJSON ./hashes.json;
   useUvPip = true;
+
+  buildInputs = [
+    libz
+  ];
 
   inherit python3;
 }
