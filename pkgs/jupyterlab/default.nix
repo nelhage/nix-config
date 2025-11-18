caebe107f4ba75011ccd2ea56f1df532ce3abbe8
Nelson Elhage <nelhage@nelhage.com>
2025-11-17 20:21:37 -0800

    Switch jupyterlab to use hashes.json

--- pkgs/jupyterlab/default.nix
+++ pkgs/jupyterlab/default.nix
@@ -1,13 +1,15 @@
-{ callPackage, python3, ... }:
+{
+  callPackage,
+  python3,
+  lib,
+  ...
+}:
 callPackage ../../lib/build-uv-package.nix {
   name = "jupyterlab-env";
   version = "0.1.0";
   src = ./.;
 
-  packagesHash = {
-    x86_64-linux = "sha256-I83+jLvyZbjBW3hTuhAHry+k4j+4UFVEpiIyZ0USQDM=";
-    aarch64-darwin = "sha256-NQmo/BDlF2ODf6cBq8DiH/c+pwn2kAMOQ4/eoqYTv2M=";
-  };
+  packagesHash = lib.trivial.importJSON ./hashes.json;
   useUvPip = true;
 
   inherit python3;
