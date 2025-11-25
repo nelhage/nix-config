15702dc37d88407bf53b612d142088095c1e39a0
Nelson Elhage <nelhage@nelhage.com>
2025-11-25 13:07:10 -0800

    Add ssh access to my laptop

--- darwin/mythique.nix
+++ darwin/mythique.nix
@@ -8,6 +8,19 @@
     ./home-manager.nix
   ];
 
+  services.openssh = {
+    enable = true;
+    extraConfig = ''
+      AuthenticationMethods publickey
+      AllowUsers nelhage
+    '';
+  };
+
+  users.users.nelhage.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILAc21zLDUk1y2VP2AIUtKhGT5SUrmPN0xI4nFn7bqmU nelhage@pixel-ten"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFzLlWzdMDU5vbNJNEneUfSoOtMz7xEzfVnfFTvLl/atHO8qKBn97IwmOZwnnxYhEOfnbHk0JB/mA083yQQ2w+M= nelhage@anthropic-laptop"
+  ];
+
   home-manager.users.nelhage =
     { ... }:
     {
