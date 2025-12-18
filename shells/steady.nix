1a7e9a42a97d79fe53aeb10c552395db72ac8065
Nelson Elhage <nelhage@nelhage.com>
2025-12-18 14:51:57 -0800

    bubblewrap is Linux-only

--- shells/steady.nix
+++ shells/steady.nix
@@ -3,15 +3,23 @@
   mkShell,
 }:
 mkShell {
-  packages = [
-    pkgs.cargo
-    pkgs.rustc
-    pkgs.rust-analyzer
-    pkgs.rustfmt
-    pkgs.clippy
-    pkgs.bubblewrap
-    pkgs.socat
-  ];
+  packages =
+    [
+      pkgs.cargo
+      pkgs.rustc
+      pkgs.rust-analyzer
+      pkgs.rustfmt
+      pkgs.clippy
+      pkgs.socat
+    ]
+    ++ (
+      if pkgs.hostPlatform.isLinux then
+        [
+          pkgs.bubblewrap
+        ]
+      else
+        [ ]
+    );
 
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
