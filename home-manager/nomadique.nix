e4f990a2a0ad4b4ec9dad2a90401017889cd323b
Nelson Elhage <nelhage@nelhage.com>
2026-03-23 12:36:58 -0700

    nomadique: fira-code

--- home-manager/nomadique.nix
+++ home-manager/nomadique.nix
@@ -1,12 +1,16 @@
 { pkgs, ... }:
 {
-    imports = [
+  imports = [
     ../modules/common.nix
     ./home.nix
     ./laptop.nix
   ];
 
   home.homeDirectory = "/home/nelhage/";
+  fonts.fontconfig.enable = true;
+  home.packages = [
+    pkgs.nerd-fonts.fira-code
+  ];
 
   nix.package = pkgs.nix;
 }
