8a9f115dd32a2e40e24f34f994de1e59116096a9
Nelson Elhage <nelhage@nelhage.com>
2026-06-30 15:41:39 -0700

    quintique: Enable syncthing

--- host/quintique/home-nelhage.nix
+++ host/quintique/home-nelhage.nix
@@ -20,4 +20,23 @@
     c.ServerApp.allow_remote_access = True
     c.ServerApp.trust_xheaders = True
   '';
+
+  services.syncthing = {
+    enable = true;
+    settings.folders = {
+      "${config.home.homeDirectory}/Sync" = {
+        id = "default";
+      };
+    };
+
+    settings.devices = {
+      hw4 = {
+        addresses = [
+          "tcp://100.78.93.125:22000/"
+        ];
+        id = "UFKNXH2-ACWP52M-U5CVTSM-2OVDJOP-6YRP75Z-SD35MLQ-ULYFZDZ-Q5MUAQC";
+        autoAcceptFolders = true;
+      };
+    };
+  };
 }
